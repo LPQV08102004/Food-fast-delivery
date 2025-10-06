738fab91691dfb34663e832337320729d0155971
Lê Phạm Quốc Việt <138115006+LPQV08102004@users.noreply.github.com>
2025-10-06 16:08:01 +0700

    commit structure service

---
+++ eureka-service/src/test/java/vn/cnpm/eureka_service/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.cnpm.eureka_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
